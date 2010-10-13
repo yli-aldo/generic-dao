9e1dcfa57677fd4250a852f4f079764e98a52ec4
simon.lg <simon.lg@700af151-df39-0410-aca5-d5d6e6a074db>
2010-10-13 20:01:33 +0000

    Issue 7
    Removed the annotation @PersistenceContext from GenericDao to support multiple persistence unit. Added CdiGenericDao using @PersistenceContext. To have the same behavior as before, use CdiGenericDao instead of GenericDao.

--- trunk/generic-dao/src/test/java/com/szczytowski/genericdao/test/ComplexDaoImpl.java
+++ trunk/generic-dao/src/test/java/com/szczytowski/genericdao/test/ComplexDaoImpl.java
@@ -1,64 +1,67 @@
 package com.szczytowski.genericdao.test;
 
-import com.szczytowski.genericdao.criteria.Criteria;
-import com.szczytowski.genericdao.impl.GenericDao;
 import java.util.List;
+
 import javax.persistence.Query;
+
+import com.szczytowski.genericdao.api.IDao;
+import com.szczytowski.genericdao.criteria.Criteria;
+import com.szczytowski.genericdao.impl.CdiGenericDao;
 
 /**
  * {@link IDao} implementation used in unit tests.
  */
-public class ComplexDaoImpl extends GenericDao<ComplexEntityImpl, Long> {
+public class ComplexDaoImpl extends CdiGenericDao<ComplexEntityImpl, Long> {
 
-    /**
-     * Find by criteria.
-     *
-     * @param criteria criteria
-     * @return results
-     */
-    public List getByCriteria(Criteria criteria) {
-        return findByCriteria(criteria);
-    }
-    
-    /**
-     * Find unique by criteria.
-     *
-     * @param criteria criteria
-     * @return result
-     */
-    public Object getUniqueByCriteria(Criteria criteria) {
-        return findUniqueByCriteria(criteria);
-    }
-    
-    /**
-     * Find by query.
-     *
-     * @param eql eql
-     * @return results
-     */
-    public List getByQuery(String eql, Object... params) {
-        Query query = getEntityManager().createQuery(eql);
-        
-        for(int i = 0; i < params.length; i++) {        
-            query.setParameter(i+1, params[i]);
-        }
-        
-        return query.getResultList();
+  /**
+   * Find by criteria.
+   * 
+   * @param criteria criteria
+   * @return results
+   */
+  public List getByCriteria(Criteria criteria) {
+    return findByCriteria(criteria);
+  }
+
+  /**
+   * Find unique by criteria.
+   * 
+   * @param criteria criteria
+   * @return result
+   */
+  public Object getUniqueByCriteria(Criteria criteria) {
+    return findUniqueByCriteria(criteria);
+  }
+
+  /**
+   * Find by query.
+   * 
+   * @param eql eql
+   * @return results
+   */
+  public List getByQuery(String eql, Object... params) {
+    Query query = getEntityManager().createQuery(eql);
+
+    for (int i = 0; i < params.length; i++) {
+      query.setParameter(i + 1, params[i]);
     }
 
-    /**
-     * Find unique by query.
-     *
-     * @param eql eql
-     * @return result
-     */
-    public Object getUniqueByQuery(String eql, Object... params) {
-        Query query = getEntityManager().createQuery(eql);
-        
-        for(int i = 0; i < params.length; i++) {        
-            query.setParameter(i+1, params[i]);
-        }
-        
-        return query.getSingleResult();
+    return query.getResultList();
+  }
+
+  /**
+   * Find unique by query.
+   * 
+   * @param eql eql
+   * @return result
+   */
+  public Object getUniqueByQuery(String eql, Object... params) {
+    Query query = getEntityManager().createQuery(eql);
+
+    for (int i = 0; i < params.length; i++) {
+      query.setParameter(i + 1, params[i]);
     }
+
+    return query.getSingleResult();
+  }
 }
