24a966add86bad6dfcc5a8c813b3038853ee64dd
simon.lg <simon.lg@700af151-df39-0410-aca5-d5d6e6a074db>
2010-11-04 15:07:10 +0000

    [maven-release-plugin]  copy for tag genericdao-1.1

---
+++ tags/genericdao-1.1/trunk/generic-dao/src/main/java/com/szczytowski/genericdao/criteria/projection/SimpleProjection.java
@@ -0,0 +1,28 @@
+package com.szczytowski.genericdao.criteria.projection;
+
+import com.szczytowski.genericdao.criteria.Criteria;
+import com.szczytowski.genericdao.criteria.Projection;
+
+/**
+ * Abstract projection.
+ *
+ * @author Maciej Szczytowski <mszczytowski-genericdao@gmail.com>
+ * @since 1.0
+ */
+public abstract class SimpleProjection implements Projection {
+
+    /**
+     * Group flag, true when this projection is grouping one.
+     */
+    protected boolean grouped;
+
+    @Override
+    public String toGroupSqlString(Criteria criteria, Criteria.CriteriaQuery criteriaQuery) {
+        throw new UnsupportedOperationException("not a grouping projection");
+    }
+
+    @Override
+    public boolean isGrouped() {
+        return grouped;
+    }
+}
---
+++ tags/genericdao-1.1/trunk/generic-dao/src/main/java/com/szczytowski/genericdao/impl/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * This package contains implementations of <code>IDao</code> interfaces. This class can be used as abstract DAO.
+ * 
+ * @author Maciej Szczytowski <mszczytowski-genericdao@gmail.com>
+ * @see com.szczytowski.genericdao.api.IDao
+ * @since 1.0
+ */
+package com.szczytowski.genericdao.impl;
