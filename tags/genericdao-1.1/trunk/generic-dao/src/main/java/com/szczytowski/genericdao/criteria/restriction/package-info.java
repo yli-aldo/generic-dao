85e1822aeb004d8d1cbe2a2cb068d0ddc3dbd70e
simon.lg <simon.lg@700af151-df39-0410-aca5-d5d6e6a074db>
2010-11-04 15:33:36 +0000

    Deleting incorrect tag

--- tags/genericdao-1.1/trunk/generic-dao/src/main/java/com/szczytowski/genericdao/criteria/restriction/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * This package contains implementations of <code>Criterion</code> interfaces.
- * 
- * @author Maciej Szczytowski <mszczytowski-genericdao@gmail.com>
- * @see com.szczytowski.genericdao.criteria.Criterion
- * @since 1.0
- */
-package com.szczytowski.genericdao.criteria.restriction;
