3e99bdd0a727eb1737758e780f4a616c7ca17cc8
simon.lg@gmail.com <simon.lg@gmail.com@700af151-df39-0410-aca5-d5d6e6a074db>
2011-07-06 14:46:04 +0000

    Removed incorrect tag

--- tags/genericdao-1.1.1/src/main/java/com/szczytowski/genericdao/impl/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * This package contains implementations of <code>IDao</code> interfaces. This class can be used as abstract DAO.
- * 
- * @author Maciej Szczytowski <mszczytowski-genericdao@gmail.com>
- * @see com.szczytowski.genericdao.api.IDao
- * @since 1.0
- */
-package com.szczytowski.genericdao.impl;
